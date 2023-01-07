12b059ae20dccfd12cd1c38097a9259a4b9c20f8
maheshkhatal27 <mbkhatal@gmail.com>
2023-01-07 21:47:17 +0530

    final version

--- src/components/DisplayUsers/index.js
+++ src/components/DisplayUsers/index.js
@@ -1,8 +1,6 @@
 import "./index.css"
-
 const DisplayUsers=props=>{
-    const {userDetails,deleteUser}=props
-
+   const {userDetails,deleteUser}=props
     const {id,name,signIn,role,count} = userDetails
     const onClickDelete=()=>{
             deleteUser(id)
@@ -11,13 +9,14 @@ const DisplayUsers=props=>{
     return(
            <tbody>
                 <tr className="data">
-                    <td>{count}</td>
+                    <td className="row-data">{id}</td>
                     <td>{name}</td>
                     <td>{signIn}</td>
                     <td>{role}</td>
                     <td><button type="button" className="del-button" onClick={onClickDelete}><img src="https://res.cloudinary.com/dodmtflaq/image/upload/v1672634552/delete-128_owa2q8.png" alt="delete" className="img" /></button></td>
                 </tr>
-           </tbody> 
+                </tbody>
+           
     )
 
 }
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,20 +2,22 @@ import { Component } from "react"
 import "./index.css"
 import Navbar from "../Navbar"
 import DisplayUsers from "../DisplayUsers"
-
 import {v4 as uuidv4} from 'uuid'
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
+import { RiAdminLine } from "react-icons/ri";
 
+const pageSize=5;
 class Home extends Component{
-    state = {isColorOn:false,userData:[],username:"",signIn:"",role:"",count:1}
+    state = {isColorOn:false,prodTab:false,salesTab:false,demoTab:false,custTab:false,scriptTab:false,
+      userData:[],username:"",signIn:"",role:"Admin",count:1,currentPage:1}
+    
+
 
      getData=()=>{
-         
         const userRecords = JSON.parse(localStorage.getItem('userList')) || []
          // console.log(userData)
          this.setState({userData:[...userRecords]})
-
     }
 
     componentDidMount(){
@@ -23,11 +25,39 @@ class Home extends Component{
     }
 
     setColor=()=>{
-        const {isColorOn}=this.state
-         // this.setState({isColorOn:true})
-        this.setState(prevState => ({isColorOn: !prevState.isColorOn}))
+        const {prodTab,salesTab,demoTab,scriptTab,custTab}=this.state
+        this.setState({isColorOn:true,prodTab:false,salesTab:false,demoTab:false,scriptTab:false,custTab:false})
+       // this.setState(prevState => ({isColorOn: !prevState.isColorOn}))
         //console.log(isColorOn)
     }
+    setProdColor=()=>{
+      const {prodTab}=this.state
+      this.setState({isColorOn:false,prodTab:true,salesTab:false,demoTab:false,scriptTab:false,custTab:false})
+      
+  }
+
+  
+  setDemoColor=()=>{
+    const {demoTab}=this.state
+    this.setState({isColorOn:false,prodTab:false,salesTab:false,demoTab:true,scriptTab:false,custTab:false})
+      
+}
+setScriptColor=()=>{
+  const {scriptTab}=this.state
+  this.setState({isColorOn:false,prodTab:false,salesTab:false,demoTab:false,scriptTab:true,custTab:false})
+      
+}
+
+setCustColor=()=>{
+  const {custTab}=this.state
+  this.setState({isColorOn:false,prodTab:false,salesTab:false,demoTab:false,scriptTab:false,custTab:true})
+      
+}
+setSalesColor=()=>{
+  const {salesTab}=this.state
+  this.setState({isColorOn:false,prodTab:false,salesTab:true,demoTab:false,scriptTab:false,custTab:false})
+      
+}
 
     deleteUser=(id)=>{
         //console.log(`to be deleted: ${id}`)
@@ -37,29 +67,56 @@ class Home extends Component{
       userData: filteredUserList,
     })
     }
+    handlePageChange = page => {
+      this.setState({ currentPage: page });
+    };
 
-     addUserDetails=(event)=>{
-        event.preventDefault()
+    handlePrevious=()=>{      
+      const{currentPage}=this.state
+      const numberOfPages=Math.ceil(this.state.userData.length/pageSize)
+      
+      if(currentPage > 1){
+        this.setState(prev=>({
+          currentPage:prev.currentPage - 1
+        }))
+      }
+      
+
+    }
+    
+    handleNext=()=>{
+      const{currentPage}=this.state
+      const numberOfPages=Math.ceil(this.state.userData.length/pageSize)
+      
+      if(currentPage < numberOfPages){
+        this.setState(prev=>({
+          currentPage:prev.currentPage + 1
+        }))
+      }
+    }
+
+    onClickAddUser=(event)=>{
+       event.preventDefault()
         const {username,signIn,role,userData,count}=this.state
         
+       const userId=uuidv4().slice(0,5)
+       
         const addUser = {
-            id: uuidv4(),
+            id: userId,
             name:username,
             signIn,
             role,
             count,
           }
           
-         
           this.setState(prevState => ({
             userData: [...prevState.userData, addUser],
             username: '',
             signIn: '',
-            role:"",
-            count: prevState.count + 1,
+            role:"Admin",
+            count:prevState.count + 1,
           }))
            
-          console.log(count)
         localStorage.setItem("userList",JSON.stringify(userData))
     }
 
@@ -75,14 +132,36 @@ class Home extends Component{
 
         this.setState({role:event.target.value})
     }
+
     
 
     render(){
-        const {isColorOn}=this.state
+        const {isColorOn,currentPage}=this.state
         const activateTab = isColorOn ? "bg-color" : ""
+        const activateProdTab = this.state.prodTab ? "bg-color" : ""
+        const activateSalesTab = this.state.salesTab ? "bg-color" : ""
+        const activateCustTab = this.state.custTab ? "bg-color" : ""
+        const activateScriptTab = this.state.scriptTab ? "bg-color" : ""
+        const activateDemoTab = this.state.demoTab ? "bg-color" : ""
         const {userData}=this.state
         const {username,signIn,role,count} =this.state
        // console.log(userData)
+       const pageCount = Math.ceil(userData.length / pageSize);
+      // console.log(pageCount)
+
+      // if (pageCount === 1) return null;
+       const pages = [];
+    for (let i = 1; i <= pageCount; i++) {
+      pages.push(
+        <li
+          className={`each-page ${i === currentPage ? 'active' : ''}`}
+          key={i}
+          onClick={() => this.handlePageChange(i)}
+        >
+          {i}
+        </li>
+      );
+    }
         
         return(
 
@@ -90,26 +169,41 @@ class Home extends Component{
                 <Navbar />
                 <div className="home-pg">
                 <div className="left-bar-container">
-                    <h1 className="items">Products</h1>
-                    <h1 className="items">Demo Script</h1>
-                    <h1 className="items">Customers</h1>
-                    <h1 className="items">Sales Team</h1>
-                    <h1 className="items">Demos</h1>
-                    <h1 className={`items ${activateTab}`} onClick={this.setColor}>  Settings</h1>
+                    <h1 className={`items ${activateProdTab}`} onClick={this.setProdColor}>Products</h1>
+                    <h1 className={`items ${activateScriptTab}`} onClick={this.setScriptColor}>Demo Script</h1>
+                    <h1 className={`items ${activateCustTab}`} onClick={this.setCustColor}>Customers</h1>
+                    <h1 className={`items ${activateSalesTab}`} onClick={this.setSalesColor}>Sales Team</h1>
+                    <h1 className={`items ${activateDemoTab}`} onClick={this.setDemoColor}>Demos</h1>
+                    <h1 className={`items ${activateTab}`} onClick={this.setColor}>Settings</h1>
                 </div>
                 
                 {isColorOn ? (<div>
-    <Popup trigger={<button type="button" className="add-user-button">ADD USER</button>} 
+    <Popup trigger={<button type="button" className="add-role-user">ADD USER</button>} 
      position="right center">
-      <form onSubmit={this.addUserDetails}>
-        <label htmlFor='username'>User Name:</label>
-        <input type="text" id="username" value={username} onChange={this.addForName} />
-        <label htmlFor='signin'>Signed In Time:</label>
-        <input type="text" id="signin" value={signIn} onChange={this.addForSignIn}  />
-        <label htmlFor='rolename'>Role Name</label>
-        <input type="text" id="rolename" value={role} onChange={this.addForRole}  />
-        <button type="submit" className="add-user-button">SUBMIT</button>
-      </form>
+            <div className="popup-container">
+                <div className="role-logo-container">
+                    <RiAdminLine size={150}/>
+                    <p className="lorem-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                </div>
+                <div className="user-info-container">
+                    <h1 className="user-information">User Information</h1>
+                    <form onSubmit={this.onClickAddUser} className="form-container">
+                      <label htmlFor="emailid" className="email-label">Email id of user</label>
+                      <input type="text" id="emailid" className="email-input" value={username} onChange={this.addForName} />
+                      <label htmlFor='signin'>Signed In Time:</label>
+                      <input type="text" id="signin" className="sign-in-input" value={signIn} onChange={this.addForSignIn}  />
+                      <label htmlFor="role" className="role-label">Role</label>
+                          <select name="role" id="role" value={role} className="role-input" onChange={this.addForRole} >
+                              <option value="Admin">Admin</option>
+                              <option value="Owner">Owner</option>
+                              <option value="Sales">Sales</option>
+                          </select>
+                      <div className="button-container">
+                        <button type="submit" className="button-add">ADD</button>
+                      </div>
+                   </form> 
+                </div>
+            </div>
     </Popup>
   </div>) :"" }
                 {isColorOn ? (<div className="table-box">
@@ -120,36 +214,27 @@ class Home extends Component{
                                     <th className="area">User</th>
                                     <th className="area">Last Signed In</th>
                                     <th className="area">Role</th>
-                                    <th className="area"></th>
+                                    <th className="delete-area"></th>
                                 </tr>
                             </thead>
                             
-          {userData.map(eachUser => (
+                            {userData.slice((currentPage - 1) * pageSize,
+              currentPage * pageSize).map(eachUser => (
             <DisplayUsers
               key={eachUser.id}
               userDetails={eachUser}
               deleteUser={this.deleteUser}
-    
             />
           ))}
-          
-                                
-                            </table>
+		                        </table>
                    
-                            <div className="pagination-container">
-                    <p className="direction">Previous</p>
-                    <button type="button" className="number-button btn-1">1</button>
-                    <button type="button" className="number-button btn-2">2</button>
-                    <p className="direction">Next</p>
-                </div> 
+                            <ul className="pages">
+                            <li><button className="button-action" onClick={this.handlePrevious}>Previous</button></li>
+                            {pages}
+                            <li><button className="button-action" onClick={this.handleNext}>Next</button></li>
+                            </ul>                
                    </div>) : ""}
-
-               
-                   
-                 
-
                 </div>
-                 
             </>
         )
     }
